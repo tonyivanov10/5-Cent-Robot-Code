3e0d113862eef9b5cb96bffd225d748cf32b8acf
tannerpapiernik <tanner.papiernik@icloud.com>
2024-02-28 10:40:06 -0500

    Checkpoint 1 navigation

--- Robot Code/MotorTest/main.cpp
+++ Robot Code/MotorTest/main.cpp
@@ -22,7 +22,8 @@
 
 #define redMinRange 0
 #define redMaxRange 3.3
-#define DEREES_PER_SECOND 65.5
+#define DEREES_PER_SECOND 47.2
+#define INCHES_PER_SECOND 8.74613
 #define ch1T1Angle 30
 #define ch1M1Duration 1
 #define ch1M1Power 30
@@ -37,22 +38,39 @@
 #define ch1M4Duration 1
 #define ch1M4Power 30
 
-FEHMotor right_motor(FEHMotor::Motor2,9.0);
-FEHMotor left_motor(FEHMotor::Motor3,9.0);
+FEHMotor right_motor(FEHMotor::Motor2,7.2);
+FEHMotor left_motor(FEHMotor::Motor3,7.2);
 DigitalInputPin sensor(FEHIO::P0_0);
 
-void movement(double duration, int power){
-    right_motor.SetPercent(-power);
-    left_motor.SetPercent(power);
-    Sleep(duration);
+void movement(double distance){
+    double time;
+    if (distance < 0){
+        time = -distance / INCHES_PER_SECOND;
+        right_motor.SetPercent(70);
+        left_motor.SetPercent(-70);
+    }
+    else{
+        time = distance / INCHES_PER_SECOND;
+        right_motor.SetPercent(-70);
+        left_motor.SetPercent(70);
+    }
+    Sleep(time);
     right_motor.Stop();
     left_motor.Stop();
 }
 void turn(double angle){
-    double time = angle / DEREES_PER_SECOND;
+    double percent;
+    double time;
+    if (angle < 0) {
+        percent = -26.5;
+        time = -angle / DEREES_PER_SECOND;
+    } else {
+        percent = 23.5;
+        time = angle / DEREES_PER_SECOND;
+    }
 
-    right_motor.SetPercent(25);
-    left_motor.SetPercent(25);
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
     Sleep(time);
     right_motor.Stop();
     left_motor.Stop();
@@ -60,23 +78,55 @@ void turn(double angle){
 
 void checkpointOne(){
     while(!(redMinRange < sensor.Value() && sensor.Value() < redMaxRange)){}
-    turn(ch1T1Angle);
-    movement(ch1M1Duration,ch1M1Power);
-    turn(ch1T2Angle);
-    movement(ch1M2Duration,ch1M2Power);
-    turn (ch1T3Angle);
-    movement(ch1M3Duration,ch1M3Power);
-    turn(ch1T4Angle);
-    turn(ch1T5Angle);
-    movement(ch1M4Duration,ch1M4Power);
 }
 
-int main(){
-    Sleep(5.0);
-    right_motor.SetPercent(-45);
-    left_motor.SetPercent(40);
-    Sleep(15.0);
+void getUpRamp(){
+    right_motor.SetPercent(-93);
+    left_motor.SetPercent(90);
+    Sleep(2.0);
     right_motor.Stop();
     left_motor.Stop();
+}
 
+
+int main(){
+    // Sleep(5.0);
+    // movement(14);
+    // Sleep(1.0);
+    // turn(-45);
+    // Sleep(1.0);
+    // movement(8.5);
+    // Sleep(1.0);
+    // turn(90);
+    // Sleep(1.0);
+    // movement(6.5);
+    // getUpRamp();
+
+    Sleep(5.0);
+    movement(5.75);
+    Sleep(0.5);
+    turn(135);
+    Sleep(0.5);
+    movement(8);
+    turn(-90);
+    Sleep(0.5);
+    movement(8);
+    getUpRamp();
+    movement(1);
+    turn(-20);
+    movement(4);
+    turn(-70);
+    Sleep(0.5);
+    movement(6.5);
+    Sleep(0.5);
+    turn(90);
+    Sleep(0.5);
+    movement(20);
+
+
+
+    // Sleep(4.0);
+    // turn(-90);
+    // Sleep(1.0);
+    // turn(90);
 }
