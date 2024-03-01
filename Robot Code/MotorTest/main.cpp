f4584dae2d56b714b5212816c22d4342537db63a
tonyivanov10 <ivanov.35@buckeyemail.osu.edu>
2024-03-01 12:25:18 -0500

    test branch for anything

--- Robot Code/MotorTest/main.cpp
+++ Robot Code/MotorTest/main.cpp
@@ -1,11 +1,3 @@
-/********************************/
-/*      Proteus Test Code       */
-/*     OSU FEH Spring 2020      */
-/*        Drew Phillips         */
-/*    02/03/20  Version 3.0.1     */
-/********************************/
-
-// AM 02/03/20
 
 /* Include preprocessor directives */
 #include <FEHLCD.h>
@@ -20,8 +12,6 @@
 #include <string.h>
 #include <stdio.h>
 
-#define redMinRange 0
-#define redMaxRange 3.3
 #define DEREES_PER_SECOND 47.2
 #define INCHES_PER_SECOND 8.74613
 #define ch1T1Angle 30`
@@ -47,76 +37,14 @@ FEHMotor left_motor(FEHMotor::Motor3,7.2);
 AnalogInputPin sensor(FEHIO::P0_0);
 DigitalEncoder right_encoder(FEHIO::P0_5);
 DigitalEncoder left_encoder(FEHIO::P0_3);
-
-
-
-void movement(double distance){
-    double revolutions = distance / CIRCUMFERENCE;
-    double tics = revolutions * 360 / DEGREES_PER_TIC;
-    double percent = 70;
-
-    if(distance < 0) {
-        tics *= -1;
-        percent *= -1;
-    }
-
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
-
-    right_motor.SetPercent(-percent);
-    left_motor.SetPercent(percent);
-
-    while(right_encoder.Counts() < tics && left_encoder.Counts() < tics);
-
-    right_motor.Stop();
-    left_motor.Stop();
-}
-void turn(double angle){
-    double tics = angle / DEGREES_PER_TIC;
-    double percent = 25;
-    if (angle < 0) {
-        tics += -1;
-        percent *= -1;
-    }
-
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
-
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(percent);
-    
-    while(right_encoder.Counts() < tics && left_encoder.Counts() < tics);
-
-    right_motor.Stop();
-    left_motor.Stop();
-}
-
+DigitalEncoder right_dir(FEHIO::P0_4);
+DigitalEncoder left_dir(FEHIO::P0_2);
 
 int main(){
-    while(!(redMinRange < sensor.Value() && sensor.Value() < redMaxRange)){}
+    //while(!(redMinRange < sensor.Value() && sensor.Value() < redMaxRange)){}
 
-    Sleep(5.0);
-    movement(5.75);
-    Sleep(0.5);
-
-    movement(5.75);
-    Sleep(0.5);
-    turn(135);
-    Sleep(0.5);
-    movement(8);
-    turn(-90);
-    Sleep(0.5);
-    movement(8);
-    // getUpRamp();
-    movement(20);
-    movement(1);
-    turn(-20);
-    movement(4);
-    turn(-70);
-    Sleep(0.5);
-    movement(6.5);
-    Sleep(0.5);
-    turn(90);
-    Sleep(0.5);
-    movement(20);
+    LCD.WriteLine("Right: " + right_encoder.Counts());
+    LCD.WriteLine("Right: " + right_dir.Counts());
+    LCD.WriteLine("Left: " + left_encoder.Counts());
+    LCD.WriteLine("Left: " + left_dir.Counts());
 }
