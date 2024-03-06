0809dabea6bb7c7cfd3808e4d7774bcb0ac70967
tonyivanov10 <ivanov.35@buckeyemail.osu.edu>
2024-03-06 12:13:20 -0500

    encoder modification back to normal

--- Robot Code/MotorTest/main.cpp
+++ Robot Code/MotorTest/main.cpp
@@ -20,11 +20,6 @@
 #include <string.h>
 #include <stdio.h>
 
-<<<<<<< HEAD
-#define redMinRange .3
-#define redMaxRange .75
-=======
->>>>>>> 70e03e1bc7823b4c92fd2fe9a89d43a33cd895de
 #define DEREES_PER_SECOND 47.2
 #define INCHES_PER_SECOND 8.74613
 #define DEGREES_PER_TIC 2
@@ -36,15 +31,11 @@
 
 FEHMotor right_motor(FEHMotor::Motor1,7.2);
 FEHMotor left_motor(FEHMotor::Motor3,7.2);
-<<<<<<< HEAD
-AnalogInputPin CdSCell(FEHIO::P0_0);
-=======
 AnalogInputPin CdsCell(FEHIO::P0_0);
 DigitalEncoder left_encoder_dir(FEHIO::P0_7);
 DigitalEncoder left_encoder_dis(FEHIO::P1_7);
 DigitalEncoder right_encoder_dir(FEHIO::P3_0);
 DigitalEncoder right_encoder_dis(FEHIO::P3_5);
->>>>>>> 70e03e1bc7823b4c92fd2fe9a89d43a33cd895de
 
 void movement(double distance){
 
@@ -90,16 +81,8 @@ void turn(double angle){
 
     //Formatting if the angle was negative.
     if (angle < 0) {
-<<<<<<< HEAD
-        percent = -26.2;
-        time = -angle / DEREES_PER_SECOND;
-    } else {
-        percent = 24.2;
-        time = angle / DEREES_PER_SECOND;
-=======
         tics *= -1;
         percent *= -1;
->>>>>>> 70e03e1bc7823b4c92fd2fe9a89d43a33cd895de
     }
 
     //Setting the encoder counts to zero.
@@ -118,35 +101,6 @@ void turn(double angle){
     left_motor.Stop();
 }
 
-<<<<<<< HEAD
-void checkpointOne(){
-    while(!(redMinRange < CdSCell.Value() && CdSCell.Value() < redMaxRange)){}
-}
-
-void getUpRamp(){
-    right_motor.SetPercent(-93);
-    left_motor.SetPercent(90);
-    Sleep(2.0);
-    right_motor.Stop();
-    left_motor.Stop();
-}
-void getDownRamp(){
-    right_motor.SetPercent(-23);
-    left_motor.SetPercent(20);
-    Sleep(2.0);
-    right_motor.Stop();
-    left_motor.Stop();
-}
-
-
-int main(){
-
-    while(!(redMinRange < CdSCell.Value() && CdSCell.Value() < redMaxRange)){
-        LCD.WriteLine(CdSCell.Value());
-    }
-
-    Sleep(1.0);
-=======
 //Main function
 /**
  * NOTE: The theoretical counts per inch is a little too high causing the movement to overshoot 
@@ -158,7 +112,6 @@ int main(){
     while(!(redMinRange < CdsCell.Value() && CdsCell.Value() < redMaxRange)){}
 
     //Move forward 5.75 inches.
->>>>>>> 70e03e1bc7823b4c92fd2fe9a89d43a33cd895de
     movement(5.75);
     Sleep(0.5);
 
@@ -176,41 +129,18 @@ int main(){
 
     //Move forward 8 inches.
     movement(8);
-<<<<<<< HEAD
-    getUpRamp();
-    movement(3);
-
-    turn(92);
-    Sleep(0.5);
-    movement(-6.5);
-    Sleep(0.5);
-=======
 
     //Move forward (up the ramp) 20 inches.
     movement(21);
     Sleep(.5);
 
     //Turn left 20 degree.
->>>>>>> 70e03e1bc7823b4c92fd2fe9a89d43a33cd895de
     turn(90);
     Sleep(.5);
 
     //Move forward 6.5 inches.
     movement(-7);
     Sleep(0.5);
-<<<<<<< HEAD
-    movement(-20);
-
-    movement(14);
-    turn(90);
-    movement(12.5);
-    turn(-70);
-    movement(3);
-    turn(-20);
-    movement(25);
-    getDownRamp();
-
-=======
 
     //Turn right 90 degrees.
     turn(-90);
@@ -219,5 +149,4 @@ int main(){
     //Move forward 20 inches (hit the boarding pass section).
     movement(-22);
     
->>>>>>> 70e03e1bc7823b4c92fd2fe9a89d43a33cd895de
 }
